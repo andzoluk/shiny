03bb05b22bb56a638899f7777a7aa0d5fc0181dd
Lukasz Andrzejewski <lukasz.coding@gmail.com>
2024-02-13 23:31:06 +0100

    lesson 2

--- masteringshiny/R/lesson1.R
+++ masteringshiny/R/lesson1.R
@@ -1,18 +1,25 @@
+#page 11, ex 1
+
 ui<- fluidPage(
   
   sliderInput("one", label="if x is", min=1, max=50, value=30),
-  "then x times 5 is ",
-  textOutput("txt")
+  sliderInput("two", label = "and y is", min=1, max=50, value=5),
+  textOutput("txt"),
+  textOutput("txt2")
   
 )
 
 server<- function(input,output){
   
   output$txt <- renderText({
-    input$one * 5
+    paste0("then x times 5 is ", input$one * 5)
+  })
+  output$txt2 <- renderText({
+    paste0("then, x times y is ", input$two*input$one)
   })
   
 }
 
 
 shinyApp(ui,server)
+
